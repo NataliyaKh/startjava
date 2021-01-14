41520e38999968812cab0b900a86fd290b4f077a
Natalia.Kh <wastelin@mail.ru>
2021-01-14 20:06:00 +0300

    Коммит

---
+++ Lesson 1/Variable.java
@@ -0,0 +1,21 @@
+public class Variable {
+    public static void main(String[] args) {
+        byte core=2;
+        short windows=10;
+        int bit=64;
+        long cpu=3855;
+        float baseSpeed=1.61f;
+        double memory=3.9d;
+        char operSys='W';
+        boolean sensor=false;
+
+        System.out.println(core);
+        System.out.println(windows);
+        System.out.println(bit);
+        System.out.println(cpu);
+        System.out.println(baseSpeed);
+        System.out.println(memory);
+        System.out.println(operSys);
+        System.out.println(sensor);
+    }
+}
