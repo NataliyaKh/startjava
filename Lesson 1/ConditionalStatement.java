3dd161064051c8d53a51e1439e9a980292a3e153
NataliyaKh <75334776+NataliyaKh@users.noreply.github.com>
2021-01-16 20:08:09 +0300

    Add files via upload

---
+++ Lesson 1/ConditionalStatement.java
@@ -0,0 +1,28 @@
+public class ConditionalStatement {
+    public static void main(String[] args) {
+        byte age = 21;
+        if(age > 20) {
+            System.out.println ("Возраст больше 20");
+        }
+        char sex = 'F';
+        if(sex == 'M') {
+            System.out.println ("Мужчина");
+        }
+        if(sex != 'M') {
+            System.out.println ("Женщина");
+        }
+        float height = 1.75f;
+        if(height < 1.80) {
+            System.out.println ("Средний рост");
+        } else {
+            System.out.println ("Высокиий рост");
+            }
+        char firstLetterName = 'A';
+        if(firstLetterName == 'M') {
+            System.out.println ("Имя на M");
+        } else if (firstLetterName == 'I') {
+            System.out.println ("Имя на I");
+            } else {System.out.println ("Имя не на M и не на I");
+                }
+    }
+}
