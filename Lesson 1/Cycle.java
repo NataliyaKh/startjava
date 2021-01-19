a729f4905666e6429496bb0864c91f8b70b6593a
Natalia.Kh <wastelin@mail.ru>
2021-01-19 21:12:08 +0300

    \n и прочее

--- Lesson 1/Cycle.java
+++ Lesson 1/Cycle.java
@@ -1,25 +1,26 @@
 public class Cycle {
     public static void main(String[] args) {
-        int i;
-        for (i = 0; i <= 20; i++) {
-            System.out.println(i + " ");
-            }
-        
+        for (int i = 0; i <= 20; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.print ("\n");
+
         int j = 6;
         while (j >= -6) {
-           System.out.println(j + " ");
-           j-=2;
+           System.out.print(j + " ");
+           j -= 2;
         }
+        System.out.print ("\n");
         
         int k = 10;
         int sum = 0;
         do {
             if(k % 2 != 0) {
-            sum+=k;
+                sum += k;
             }
             k++;
-        }
-        while(k <= 20);
+        }while(
+            k <= 20);
         System.out.println("Результат сложения " + sum);
     }
 }
