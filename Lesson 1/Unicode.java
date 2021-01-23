3f41fdae1482e1cfadeb8b9965416e6e78a90851
Natalia.Kh <wastelin@mail.ru>
2021-01-23 12:02:02 +0300

    Юникод

---
+++ Lesson 1/Unicode.java
@@ -0,0 +1,9 @@
+public class Unicode {
+    public static void main(String[] args) {
+        char symbol = 33;
+        do {
+            System.out.print(symbol + " ");
+            symbol++;
+        } while(symbol < 126);
+    }
+}
