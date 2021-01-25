aca1b7d1d50cdd2b3902a34bcf96285309ae290f
Natalia.Kh <wastelin@mail.ru>
2021-01-25 17:06:27 +0300

    Добавила Person

---
+++ Lesson 2/Person.java
@@ -0,0 +1,28 @@
+public class Person {
+    char sex = 'F';
+    String name = "Isabella";
+    int height = 165;
+    int weight = 56;
+    int age = 27;
+
+    void walk() {
+
+    };
+
+    void sit() {
+
+    };
+
+    void run() {
+
+    };
+
+    void speak() {
+
+    };
+    
+    boolean learnJava() {
+        System.out.println("Вы изучаете Java");
+        return true;
+    };
+}
