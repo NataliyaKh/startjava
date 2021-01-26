c393009560e8afb32846bcb4bf9309345a8494e6
Natalia.Kh <wastelin@mail.ru>
2021-01-26 20:37:23 +0300

    Присвоение значений

--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -1,11 +1,19 @@
 public class WolfTest {
     public static void main(String[] args) {
-        Wolf wolfOne = new Wolf ();
+        Wolf wolfOne = new Wolf();
+        
+        wolfOne.sex = 'M';
+        wolfOne.name = "White Fang";
+        wolfOne.weight = 56;
+        wolfOne.age = 3;
+        wolfOne.colour = "grey";
+
         System.out.println("sex = " + wolfOne.sex);
         System.out.println("name = " + wolfOne.name);
         System.out.println("weight = " + wolfOne.weight);
         System.out.println("age = " + wolfOne.age);
         System.out.println("colour = " + wolfOne.colour);
+
         wolfOne.walk();
         wolfOne.sit();
         wolfOne.run();
