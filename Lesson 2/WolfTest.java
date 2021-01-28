3f730372bb0b38355d24af3a93d00b6060a1dbf5
Natalia.Kh <wastelin@mail.ru>
2021-01-28 15:27:41 +0300

    вызовы методов

--- Lesson 2/WolfTest.java
+++ Lesson 2/WolfTest.java
@@ -1,21 +1,11 @@
 public class WolfTest {
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-        void setSex(char sex) {
-            this.sex = 'M';
-        }
-        void setName(String name) {
-            this.name = "White Fang";
-        }
-        void setWeight(int weight) {
-            this.weight = 56;
-        }
-        void setAge(int age) {
-            this.age = 3;
-        }
-        void setColour(String colour) {
-            this.colour = "grey";
-        }
+        wolfOne.setSex('M');
+        wolfOne.setName("White Fang");
+        wolfOne.setWeight(56);
+        wolfOne.setAge(9);
+        wolfOne.setColour("grey");
         
         System.out.println("sex = " + wolfOne.getSex());
         System.out.println("name = " + wolfOne.getName());
