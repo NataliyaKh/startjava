2509ad6695b59c3f7d2e4298c4b54ac79f069a56
Natalia.Kh <wastelin@mail.ru>
2021-02-03 00:11:35 +0300

    убрала многое

--- Lesson 2/Calculator.java
+++ Lesson 2/Calculator.java
@@ -2,70 +2,67 @@ import java.util.Scanner;
 
 public class Calculator {
     private int num1;
-    private char operation;
-    private int num2;
-    private int result;
-
+    
     public int getNum1() {
         return num1;
-    }
-
-    public int getNum2() {
-        return getNum2;
-    }
-
-    public void getOperation() {
-        return operation;
-    }
-
-    public int getResult() {
-        return result;
     }
 
     void setNum1(int num1) {
         this.num1 = num1;
     }
 
+    private int num2;
+    
+    public int getNum2() {
+        return getNum2;
+    }
+
     void setNum2(int num2) {
         this.num2 = num2;
     }
 
+    private char operation;
+    
+    public void getOperation() {
+        return operation;
+    }
+
     void setOperation (char operation) {
-        Scanner scanOperation = new Scanner(System.in);
-        System.out.println("Введите знак математической операции: ");
-        char operation = scanOperation.next();
-        switch(operation) {
-            case '+' : 
-                result = num1 + num2;
-                break;
-            case '-' : 
-                result = num1 - num2;
-                break;
-            case '*' : 
-                result = num1 * num2;
-                break;
-            case '/' : 
-                result = num1 / num2;
-                break;
-            case '^' : 
-                result = 1;
-                for (int exponent = 1; exponent <= num2; exponent++) {
-                    result *= num1;
-                }
-                break;
-            case '%' : 
-                result = num1 % num2;
-                break;
-            default : 
-                System.out.println("Знак математической операции некорректен");
-                return;
-        char setOperation() = scanOperation.next();
-        System.out.println(calculatorOne.getOperation());
-            System.out.println(num1 + " " + operation + " " + num2 + " = " + result);
-        }
         this.operation = operation;
     }
 
+    private int result;
+    switch(operation) {
+        case '+' : 
+            result = num1 + num2;
+            break;
+        case '-' : 
+            result = num1 - num2;
+            break;
+        case '*' : 
+            result = num1 * num2;
+            break;
+        case '/' : 
+            result = num1 / num2;
+            break;
+        case '^' : 
+            result = 1;
+            for (int exponent = 1; exponent <= num2; exponent++) {
+                result *= num1;
+            }
+            break;
+        case '%' : 
+            result = num1 % num2;
+            break;
+        default : 
+            System.out.println("Знак математической операции некорректен");
+            return;
+        }
+
+    public int getResult() {
+        return result;
+    }
+    
     void setResult(int result) {
         this.result = result;
     }
