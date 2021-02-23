5cd273cc59b3eaceaf1f04ee201a9045090d17bf
Natalia.Kh <wastelin@mail.ru>
2021-02-24 00:43:57 +0300

    Boolean.valueOf

--- Lesson 2/Calculator.java
+++ Lesson 2/Calculator.java
@@ -6,7 +6,7 @@ public class Calculator {
     private char operation;
     public int result;
     public String proceed;
-    public boolean proceedValid = (proceed == "да")|(proceed == "нет");
+    public boolean proceedValid = Boolean.valueOf((proceed == "да")|(proceed == "нет"));
 
     public int getNum1() {
         return num1;
