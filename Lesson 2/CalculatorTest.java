266828dc8ab649bf1efbeee6dfc85acdc1e3539f
Natalia.Kh <wastelin@mail.ru>
2021-03-07 13:36:07 +0300

    заменила кириллицу латиницей

--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -5,37 +5,41 @@ public class CalculatorTest {
         Calculator calculatorOne = new Calculator();
         Scanner scan = new Scanner(System.in);
         do {
-            System.out.println("Введите первое число: ");
-            int num1 = scan.nextInt();
-            calculatorOne.setNum1(num1);
+            String answer = "yes";
+            if (answer.equals("yes")) {
+                System.out.println("Введите первое число: ");
+                int num1 = scan.nextInt();
+                calculatorOne.setNum1(num1);
 
-            System.out.println("Введите знак математической операции: ");
-            char operation = scan.next().charAt(0);
-            calculatorOne.setOperation(operation);
+                System.out.println("Введите знак математической операции: ");
+                char operation = scan.next().charAt(0);
+                calculatorOne.setOperation(operation);
 
-            System.out.println("Введите второе число: ");
-            int num2 = scan.nextInt();
-            calculatorOne.setNum2(num2);
+                System.out.println("Введите второе число: ");
+                int num2 = scan.nextInt();
+                calculatorOne.setNum2(num2);
             
-            System.out.println(num1 + " " + operation + " " + num2 + " = " + calculatorOne.calculate() + "\n Хотите продолжить вычисления? да / нет " );
+                System.out.println(num1 + " " + operation + " " + num2 + " = " + calculatorOne.calculate());
+                System.out.println("Хотите продолжить вычисления? yes / no ");
+            }
         } while (isNext());
+        scan.close();
         System.out.println("Вычисления окончены");
     }
 
     public static boolean isNext() {
         Scanner scan = new Scanner(System.in);
-        String answer;
-        answer = scan.next();
+        String answer = scan.next();
         scan.nextLine();
-        do {
-            System.out.println("Выбранный ответ некорректен. Введите да / нет");
-            answer = scan.next();
-            scan.nextLine();
-        } while (!answer.equals("да")&!answer.equals("нет"));
-        if(answer.equals("нет")) {
+        while (!answer.equals("yes")&!answer.equals("no")) {
+           System.out.println("Выбранный ответ некорректен. Введите yes / no");
+           answer = scan.next();
+           scan.nextLine();
+        }
+        if(answer.equals("no")) {
             return false;
         } else {
-            return true;
+           return true;
         }
     }
 }
