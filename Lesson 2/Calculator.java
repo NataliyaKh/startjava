d2e23976571df0f3ef2ae3c86c8d56d552a8d8b3
Natalia.Kh <wastelin@mail.ru>
2021-03-07 14:56:12 +0300

    убрала геттеры

--- Lesson 2/Calculator.java
+++ Lesson 2/Calculator.java
@@ -3,24 +3,12 @@ public class Calculator {
     private int num2;
     private char operation;
 
-    public int getNum1() {
-        return num1;
-    }
-
     void setNum1(int num1) {
         this.num1 = num1;
     }
 
-    public int getNum2() {
-        return num2;
-    }
-
     void setNum2(int num2) {
         this.num2 = num2;
-    }
-
-    public char getOperation() {
-        return operation;
     }
 
     void setOperation(char operation) {
