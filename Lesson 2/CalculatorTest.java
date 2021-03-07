57ce62e365cda7ea14a8b726b828f8104764f093
Natalia.Kh <wastelin@mail.ru>
2021-03-07 14:05:11 +0300

    решила проблему кириллицы

--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -5,8 +5,8 @@ public class CalculatorTest {
         Calculator calculatorOne = new Calculator();
         Scanner scan = new Scanner(System.in);
         do {
-            String answer = "yes";
-            if (answer.equals("yes")) {
+            String answer = "да";
+            if (answer.equals("да")) {
                 System.out.println("Введите первое число: ");
                 int num1 = scan.nextInt();
                 calculatorOne.setNum1(num1);
@@ -20,7 +20,6 @@ public class CalculatorTest {
                 calculatorOne.setNum2(num2);
             
                 System.out.println(num1 + " " + operation + " " + num2 + " = " + calculatorOne.calculate());
-                System.out.println("Хотите продолжить вычисления? yes / no ");
             }
         } while (isNext());
         scan.close();
@@ -28,14 +27,15 @@ public class CalculatorTest {
     }
 
     private static boolean isNext() {
-        Scanner scan = new Scanner(System.in);
+        Scanner scan = new Scanner(System.in, "Cp866");
+        System.out.println("Хотите продолжить вычисления? да / нет ");
         String answer = scan.next();
         scan.nextLine();
-        while (!answer.equals("yes") && !answer.equals("no")) {
-           System.out.println("Выбранный ответ некорректен. Введите yes / no");
+        while (!answer.equals("да") && !answer.equals("нет")) {
+           System.out.println("Выбранный ответ некорректен. Введите да / нет");
            answer = scan.next();
            scan.nextLine();
         }
-        return answer.equals("yes");
+        return answer.equals("да");
     }
 }
