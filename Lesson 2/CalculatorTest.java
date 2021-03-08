509a4faaaf1d779c54be3f59bd7070bda4c38d0c
Natalia.Kh <wastelin@mail.ru>
2021-03-08 22:00:02 +0300

    убрала лишний if

--- Lesson 2/CalculatorTest.java
+++ Lesson 2/CalculatorTest.java
@@ -6,21 +6,20 @@ public class CalculatorTest {
         Scanner scan = new Scanner(System.in);
         do {
             String answer = "да";
-            if (answer.equals("да")) {
-                System.out.println("Введите первое число: ");
-                int num1 = scan.nextInt();
-                calculatorOne.setNum1(num1);
-
-                System.out.println("Введите знак математической операции: ");
-                char operation = scan.next().charAt(0);
-                calculatorOne.setOperation(operation);
-
-                System.out.println("Введите второе число: ");
-                int num2 = scan.nextInt();
-                calculatorOne.setNum2(num2);
             
-                System.out.println(num1 + " " + operation + " " + num2 + " = " + calculatorOne.calculate());
-            }
+            System.out.println("Введите первое число: ");
+            int num1 = scan.nextInt();
+            calculatorOne.setNum1(num1);
+
+            System.out.println("Введите знак математической операции: ");
+            char operation = scan.next().charAt(0);
+            calculatorOne.setOperation(operation);
+
+            System.out.println("Введите второе число: ");
+            int num2 = scan.nextInt();
+            calculatorOne.setNum2(num2);
+            
+            System.out.println(num1 + " " + operation + " " + num2 + " = " + calculatorOne.calculate());
         } while (isNext());
         scan.close();
         System.out.println("Вычисления окончены");
