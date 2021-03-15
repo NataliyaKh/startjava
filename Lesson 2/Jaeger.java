63b1a401e67ad9c76bb5e528b781ab2d1b311c46
Natalia.Kh <wastelin@mail.ru>
2021-03-15 12:31:52 +0300

    разделила методы

--- Lesson 2/Jaeger.java
+++ Lesson 2/Jaeger.java
@@ -11,48 +11,63 @@ public class Jaeger {
     public String getModelName() {
         return modelName;
     }
+
     void setModelName(String modelName) {
         this.modelName = modelName;
     }
+
     public String getMark() {
         return mark;
     }
+
     void setMark(String mark) {
         this.mark = mark;
     }
+
     public String getOrigin() {
         return origin;
     }
+
     void setOrigin(String origin) {
         this.origin = origin;
     }
+
     public float getHeight() {
         return height;
     }
+
     void setHeight(float height) {
         this.height = height;
     }
+
     public float getWeight() {
         return weight;
     }
+
     void setWeight(float weight) {
         this.weight = weight;
     }
+
     public int getSpeed() {
         return speed;
     }
+
     void setSpeed(int speed) {
         this.speed = speed;
     }
+
     public int getStrength() {
         return strength;
     }
+
     void setStrength(int strength) {
         this.strength = strength;
     }
+
     public int getArmor() {
         return armor;
     }
+    
     void setArmor(int armor) {
         this.armor = armor;
     }
