a7984fcffc9ecc77235e7f090da51d5606316b99
Natalia.Kh <wastelin@mail.ru>
2021-03-17 20:53:27 +0300

    заменила сеттеры конструкторами

--- Lesson 2/Jaeger.java
+++ Lesson 2/Jaeger.java
@@ -1,75 +1,27 @@
-public class Jaeger {
-    private String modelName;
-    private String mark;
-    private String origin;
-    private float height;
-    private float weight;
-    private int speed;
-    private int strength;
-    private int armor;
+class Jaeger {
+    String modelName;
+    String mark;
+    String origin;
+    float height;
+    float weight;
+    int speed;
+    int strength;
+    int armor;
 
-    public String getModelName() {
-        return modelName;
-    }
-
-    void setModelName(String modelName) {
+    public Jaeger (String modelName, String mark, String origin, float height, float weight, int speed, int strength, int armor) {
         this.modelName = modelName;
-    }
-
-    public String getMark() {
-        return mark;
-    }
-
-    void setMark(String mark) {
         this.mark = mark;
-    }
-
-    public String getOrigin() {
-        return origin;
-    }
-
-    void setOrigin(String origin) {
         this.origin = origin;
-    }
-
-    public float getHeight() {
-        return height;
-    }
-
-    void setHeight(float height) {
         this.height = height;
-    }
-
-    public float getWeight() {
-        return weight;
-    }
-
-    void setWeight(float weight) {
         this.weight = weight;
-    }
-
-    public int getSpeed() {
-        return speed;
-    }
-
-    void setSpeed(int speed) {
         this.speed = speed;
-    }
-
-    public int getStrength() {
-        return strength;
-    }
-
-    void setStrength(int strength) {
         this.strength = strength;
+        this.armor = armor;
     }
 
-    public int getArmor() {
-        return armor;
-    }
-    
-    void setArmor(int armor) {
-        this.armor = armor;
+    @Override
+    public String toString() {
+        return String.format("modelName = %s \nmark = %s \norigin = %s \nheight = %f \nweight = %f \nspeed = %i \nstrength = %i \narmor = %i", modelName, mark, origin, height, weight, speed, strength, armor);
     }
 
     public boolean drift() {
