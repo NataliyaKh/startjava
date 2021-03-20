fe3072062fdfbb1254ab5977f219ac4e064f23e3
Natalia.Kh <wastelin@mail.ru>
2021-03-20 10:45:50 +0300

    добавила геттеры

--- Lesson 2/Jaeger.java
+++ Lesson 2/Jaeger.java
@@ -1,12 +1,37 @@
 public class Jaeger {
-    String modelName;
-    String mark;
-    String origin;
-    float height;
-    float weight;
-    int speed;
-    int strength;
-    int armor;
+    private String modelName;
+    private String mark;
+    private String origin;
+    private float height;
+    private float weight;
+    private int speed;
+    private int strength;
+    private int armor;
+
+    public String getModelName() {
+        return modelName;
+    }
+    public String getMark() {
+        return mark;
+    }
+    public String getOrigin() {
+        return origin;
+    }
+    public float getHeight() {
+        return height;
+    }
+    public float getWeight() {
+        return weight;
+    }
+    public int getSpeed() {
+        return speed;
+    }
+    public int getStrength() {
+        return strength;
+    }
+    public int getArmor() {
+        return armor;
+    }
 
     public Jaeger (String modelName, String mark, String origin,
                     float height, float weight, int speed, int strength, int armor) {
